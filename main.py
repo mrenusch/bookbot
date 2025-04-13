d009d0848a9ae5567f70fe281261cdf6b648b07e
maciakowy <maciekrenusch@gmail.com>
2025-04-14 00:49:40 +0200

    add read a file function

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    print(get_book_text('books/frankenstein.txt'))
+
+main()
+    # print(get_book_text(books/frankenstein.txt
